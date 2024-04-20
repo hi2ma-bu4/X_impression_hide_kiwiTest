50387870c0369b0fa5187a8d72b4b95b7cf399a8
hitumabushi <138351825+hi2ma-bu4@users.noreply.github.com>
2024-04-20 10:04:00 +0900

    v1.11.4 _PCを追記

--- script.user.js
+++ script.user.js
@@ -5,7 +5,7 @@
 // @name:zh-CN          使用 "display:none;" 隐藏 Twitter（曾用名: 𝕏）的印象收益骗子。
 // @name:zh-TW          使用 "display:none;" 隱藏 Twitter（曾用名: 𝕏）的印象詐騙者。
 // @namespace           https://snowshome.page.link/p
-// @version             1.11.3
+// @version             1.11.4
 // @description         Twitterのインプレゾンビを非表示にしたりブロック・通報するツールです。
 // @description:ja      Twitterのインプレゾンビを非表示にしたりブロック・通報するツールです。
 // @description:en      A tool to hide, block, and report spam on Twitter.
@@ -284,7 +284,7 @@ Twitter(旧:𝕏)のインプレッション小遣い稼ぎ野郎どもをdispla
         "[role=alertdialog] [role=group] [role=button] div",
     ]
     const REPORT_QUERY_LIST = [
-        `${MENU_DISP_QUERY} div[role=menuitem]:has(path[d^="M3 2h18"])`,
+        `${MENU_DISP_QUERY_PC} div[role=menuitem]:has(path[d^="M3 2h18"])`,
         ["[role=radiogroup] label", 5],
         "[role=group]:has([role=radiogroup]) div[role=button]:not(:has(svg))",
         ["[role=group] div[role=button]:not(:has(svg))", 1],
